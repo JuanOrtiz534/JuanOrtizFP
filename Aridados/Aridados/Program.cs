fe7ef3dfb523d5228b19135da1437a3b3640736e
JuanOrtiz534 <jpod171107@gmail.com>
2025-08-15 15:31:30 -0500

    Add files via upload

---
+++ Aridados/Aridados/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int num1 = 0;
+int num2 = 0;
+int num3 = 0;
+
+Console.WriteLine("imgrese el numero1:");
+num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("ingrese el numeri2:");
+num2= Int32.Parse(Console.ReadLine());
