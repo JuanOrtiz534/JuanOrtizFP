8af327b7ce5c293ae82e3177f1d3b2d3138283a2
JuanOrtiz534 <jpod171107@gmail.com>
2025-08-29 15:28:43 -0500

    Add files via upload

---
+++ ciclo 2.cs
@@ -0,0 +1,25 @@
+﻿namespace _Ciclo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            int contador = 1;
+            int acumulador = 1;
+
+            Console.WriteLine("Ingrese un número");
+            num = int.Parse(Console.ReadLine());
+
+
+            while (contador <= num)
+            {
+                acumulador = acumulador * contador;
+                Console.WriteLine($"Contador:{contador} - Factorial:{acumulador}");
+                contador++;
+            }
+            Console.WriteLine($"El factorial de {num} es {acumulador}");
+        }
+    }
+}
+
