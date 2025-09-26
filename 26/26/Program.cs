ef4e885e182eb2a2ef8c5544c9f9bef0cd118a45
JuanOrtiz534 <jpod171107@gmail.com>
2025-09-26 15:28:09 -0500

    Add files via upload

---
+++ 26/26/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] Num1 = new int[2, 3];
+            int[,] Num2 = new int[2, 3];
+            int[,] Sum = new int[2, 3];
+
+            for (int i = 0; i < 3; i++) //Recorrer las filas
+            {
+                for (int j = 0; j < 2; j++)//Recorrer las columnas
+                {
+                    Console.WriteLine($"Digite el numero 1 {i},{j}:");
+                    Num1[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+        }
+    }
+}
