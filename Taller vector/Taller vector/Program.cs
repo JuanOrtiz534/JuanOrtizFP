b30783dd5d64700c151b7f3272f376ee80c09370
JuanOrtiz534 <jpod171107@gmail.com>
2025-10-01 15:12:14 -0500

    Add files via upload

---
+++ Taller vector/Taller vector/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Taller_vector
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Num=new int[15];
+            int numayor = 0;
+            int numenor = 99999;
+            for (int i = 0 ; i < Num.Length; i++)
+            {
+                Console.WriteLine("Ingrese un número");
+                Num[i] = Convert.ToInt32(Console.ReadLine());
+               
+                if (Num[i] > numayor)
+                {
+                    numayor = Num[i];
+                }
+                if (Num[i] < numenor )
+                {
+                    numenor = Num[i];
+                }
+            }
+            Console.WriteLine($"Mayor={numayor} menor={numenor}");
+
+        }
+    }
+}
