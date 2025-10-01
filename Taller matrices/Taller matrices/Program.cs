06ed653696286e399c03be6dcccf10b8b13c61ba
JuanOrtiz534 <jpod171107@gmail.com>
2025-10-01 15:33:49 -0500

    Add files via upload

---
+++ Taller matrices/Taller matrices/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Taller_matrices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] numeros = new int[5, 6];
+            for (int i = 0; i < 5;i++)
+            {
+                for(int j = 0; j < 6;j++)
+                {
+                    Console.WriteLine($"Digite el numero 1 {i},{j}:");
+                    numeros[i, j] = Convert.ToInt32(Console.ReadLine());
+                    if (j!=j++)
+                    {
+                        numero
+
+                    }
+                }
+            }
+            
+        }
+    }
+}
